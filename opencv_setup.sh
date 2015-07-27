541d300975546c3d04744136fb9f5225b0ab22db
Jack Simpson <jackbrucesimpson@gmail.com>
2015-07-27 10:48:10 +1000

    added script to install opencv + Python and it's dependencies

---
+++ opencv_setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+sudo apt-get install build-essential cmake git pkg-config
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install libgtk2.0-dev
+sudo apt-get install libatlas-base-dev gfortran
+sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+sudo apt-get install libopencv-dev python-opencv
+sudo apt-get install python-pip
+
+sudo pip install "picamera[array]"
