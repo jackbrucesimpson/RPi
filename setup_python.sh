783e5e92a0287e4f5e01462877f1b5c8596dff27
Jack Simpson <jackbrucesimpson@gmail.com>
2015-08-14 18:13:17 +1000

    update

--- setup_python.sh
+++ setup_python.sh
@@ -9,6 +9,9 @@ sudo apt-get upgrade
 sudo apt-get install build-essential python python3 python-dev python3-dev python-setuptools python3-setuptools gfortran
 
 sudo apt-get install python-pip python-numpy python-cython python-scipy python-sympy python-nose python-matplotlib python-pandas ipython ipython-notebook
-sudo apt-get install python3-pip python-numpy
+sudo apt-get install libatlas-dev libatlas3gf-base
+sudo apt-get install python-sklearn
+sudo pip install seaborn
+sudo apt-get install python3-pip python3-numpy
 apt-get install python-virtualenv virtualenvwrapper
-#sudo pip-3.2 install matplotlib pandas
+sudo pip-3.2 install matplotlib pandas scikit-learn seaborn
